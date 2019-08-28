95f34f14e6a963ff2d18a0609b0f8edf04a5a0a0
1146953160 <1146953160@qq.com>
2019-08-28 16:01:49 +0800

    systemStatusService改为Autowired

--- src/main/java/server/MonitorDataCenter.java
+++ src/main/java/server/MonitorDataCenter.java
@@ -1,15 +1,10 @@
 package server;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 import service.SystemStatusService;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
 
 //@WebServlet(name="MonitorDataCenter",urlPatterns = "/MonitorDataCenter",loadOnStartup=1)
 //标记为Servlet不是为了其被访问，而是为了便于伴随Tomcat一起启动
