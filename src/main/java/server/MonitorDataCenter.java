b15c08fe81e27c9940c15e079df91302e752144f
1146953160 <1146953160@qq.com>
2019-08-28 16:01:02 +0800

    systemStatusService改为自动注入

--- src/main/java/server/MonitorDataCenter.java
+++ src/main/java/server/MonitorDataCenter.java
@@ -1,7 +1,9 @@
 package server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 import service.SystemStatusService;
 
 import javax.annotation.PostConstruct;
@@ -9,14 +11,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 
-@WebServlet(name="MonitorDataCenter",urlPatterns = "/MonitorDataCenter",loadOnStartup=1)
+//@WebServlet(name="MonitorDataCenter",urlPatterns = "/MonitorDataCenter",loadOnStartup=1)
 //标记为Servlet不是为了其被访问，而是为了便于伴随Tomcat一起启动
+@Component
 public class MonitorDataCenter {
+    @Autowired
+    SystemStatusService systemStatusService;
 
     @PostConstruct
     public void init() {
-        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-        SystemStatusService systemStatusService = (SystemStatusService) context.getBean("systemStatusService");
         new Thread(new MonitorThread(systemStatusService)).start();
     }
 
